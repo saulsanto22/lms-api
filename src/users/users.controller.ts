599e859d96ad381e27e91f171da74198d107be5d
saulsanto22 <saul.santoa@gmail.com>
2026-09-02 11:28:29 +0700

    feat: implement roles guard and decorator, and remove unused agent and skill reference files.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,12 +2,13 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto/create-user.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RoleGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('users')
 export class UsersController {
 
     constructor(private readonly userService: UsersService) { }
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RoleGuard)
     @Get()
     async findAll() {
         return this.userService.findAll()
