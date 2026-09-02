83b4faeeead8c534bf9c37f431674166c20be0b2
saulsanto22 <saul.santoa@gmail.com>
2026-09-02 15:20:53 +0700

    feat: add Course model to Prisma schema and restrict users controller GET endpoint to ADMIN role

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,15 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto/create-user.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RoleGuard } from 'src/auth/guards/roles.guard';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/auth/decorators/roles.decorators';
 
 @Controller('users')
 export class UsersController {
 
     constructor(private readonly userService: UsersService) { }
     @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles('ADMIN')
     @Get()
     async findAll() {
         return this.userService.findAll()
